4d4a4cd7544e5ab1650aaa57c795f4044b20132e
Yang Wenwen <wenwen.yang01@sap.com>
2016-09-01 18:10:38 +0800

    enable Mongo DB, replace fake data

--- NodeJS/Movie Website/imooc/app.js
+++ NodeJS/Movie Website/imooc/app.js
@@ -1,69 +1,58 @@
 var express=require('express');
 var port= process.env.PORT || 3000;
 var path = require('path');
+var mongoose = require('mongoose');
+var _=require('underscore');
 var serveStatic=require('serve-static');
 var bodyParser=require('body-parser');
+var Movie = require('./models/movie');
+
+
 var app = express();
 
+//connect to mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/imooc',function(){
+	console.log('connection established')
+	}
+)
 
+//set static resource path
 app.set('views','./views/pages');
 app.set('view engine','jade');
 app.use(bodyParser({extended:true}));
 app.use(serveStatic(path.join(__dirname,'library')));
+app.locals.moment = require('moment');
 app.listen(port);
 
 console.log('imooc started on port'+port);
 
 //index page
 app.get('/',function(req,res){
-	res.render('index',{
-		title:'imooc 首页',
-		movies:[{
-			title:'机械战警',
-			_id:1,
-			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title:'机械战警',
-			_id:2,
-			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title:'机械战警',
-			_id:3,
-			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title:'机械战警',
-			_id:4,
-			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title:'机械战警',
-			_id:5,
-			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title:'机械战警',
-			_id:6,
-			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		}]
+		Movie.fetch(function(err, movies){
+			console.log('search done')
+			if(!err){
+				console.log(err)
+			}
+		res.render('index',{
+			title:'imooc 首页',
+			movies: movies
+		})
 	})
 })
 
 //detail page
 app.get('/movie/:id',function(req,res){
-	res.render('detail',{
-	title:'imooc 详情页',
-		movie:{
-			director:'何塞',
-			country:'美国',
-			title:'机械战警',
-			year:2014,
-			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-			language:'英语',
-			flash:'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-			summary:'This is summary blablabla...'
-		}
+	var id = req.params.id;
+	Movie.findById(id,function(err, movie){
+		res.render('detail',{
+		title:'imooc '+ movie.title,
+		movie: movie		
+		})
 	})
 })
 
-//admin page
+//admin record page
 app.get('/admin/movie',function(req,res){
 		res.render('admin',{
 		title:'imooc 后台录入',
@@ -80,20 +69,72 @@ app.get('/admin/movie',function(req,res){
 	})
 })
 
+
+
+//admin post movie (to add movie)
+app.post('/admin/movie/new',function(req,res){
+	var id = req.body.movie._id;
+	var movieObj = req.body.movie;
+	var _movie;
+
+	if(id !=='undefined'){
+		Movie.findById(id,function(err,movie){
+			if (err){
+				console.log(err)
+			}
+			_movie=_.extend(movie,movieObj);
+			_movie.save(function(err,movie){
+				if(err){
+					console.log(err)
+				}
+				res.redirect('/movie/'+movie._id)
+			})
+		})
+	}else{
+		_movie = new Movie({
+			director: movieObj.direcor,
+			title: movieObj.title,
+			year: movieObj.year,
+			language: movieObj.language,
+			summary: movieObj.summary,
+			poster: movieObj.poster,
+			flash: movieObj.flash,
+			country: movieObj.country
+		})
+
+		_movie.save(function(err,movie){
+			if(err){
+				console.log(err)
+			}
+			res.redirect('/movie/'+movie._id)
+		})
+	}
+})
+
+
 //list page
 app.get('/admin/list',function(req,res){
-	res.render('list',{
+	Movie.fetch(function(err, movies){
+		if(err){
+			console.log(err)
+		}
+		res.render('list',{
 		title:'imooc 列表页',
-		movies:[{
-			title:'机械战警',
-			_id:1,
-			director:'何塞',
-			country:'美国',
-			year:2014,
-			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-			language:'英语',
-			flash:'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-			summary:'This is summary blablabla...'
-		}]
+		movies:movies
+		})	
 	})
+})
+
+//admin update movie page
+app.post('admin/update/:id',function(req,res){
+	var id = req.params.id;
+	if(id){
+		Movie.findById(id,function(err,movie){
+			res.render('admin',{
+				title :'imooc 后台更新页',
+				movie: movie
+
+			})
+		})
+	}
 })
